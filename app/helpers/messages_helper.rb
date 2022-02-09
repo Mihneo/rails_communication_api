a7fd6f2acb8725f753a3c9154191a8ddfe8a1b54
Mihnea Stefan <smihnea64@gmail.com>
2022-02-09 15:41:09 +0200

    solve conditional pointless message creation without user check

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -3,10 +3,15 @@ module MessagesHelper
     is_successful = true
     ActiveRecord::Base.transaction do
       message_params[:receiver_ids].uniq.each do |receiver_id|
-        @message = Message.new(body: message_params[:body], sender_id: message_params[:sender_id],
-                               receiver_id: receiver_id)
+        if User.exists?(id: receiver_id)
+          @message = Message.new(body: message_params[:body], sender_id: message_params[:sender_id],
+                                 receiver_id: receiver_id)
 
-        unless User.exists?(id: receiver_id) && @message.save
+          unless @message.save
+            is_successful = false
+            raise ActiveRecord::Rollback
+          end
+        else
           is_successful = false
           raise ActiveRecord::Rollback
         end
