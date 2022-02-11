a6b50153606bb9f0c3c51019e93e85c6f3477020
Mihnea Stefan <smihnea64@gmail.com>
2022-02-11 13:25:53 +0200

    solve serialization comms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :logged_in?
+  serialization_scope :current_user
 
   def logged_in?
     render json: {}, status: :unauthorized unless session[:user_id].present?
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -2,6 +2,6 @@ class MessageSerializer < ActiveModel::Serializer
   attributes :id, :body, :sender_id, :receiver_id, :seen, :received_at
 
   def received_at
-    object.created_at.in_time_zone(User.find(object.receiver_id).timezone)
+    object.created_at.in_time_zone(current_user.timezone)
   end
 end
