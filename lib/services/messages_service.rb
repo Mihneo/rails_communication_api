73acfebaecc142bcf10ffe6a403ff90dbcb90166
Mihnea Stefan <smihnea64@gmail.com>
2022-02-11 13:41:44 +0200

    solve messages service comments'

--- lib/services/messages_service.rb
+++ lib/services/messages_service.rb
@@ -1,22 +1,20 @@
 class MessagesService
+  attr_accessor :message_params
+
   def initialize(message_params)
     @message_params = message_params
   end
 
   def send_messages
-    is_successful = true
     ActiveRecord::Base.transaction do
-      @message_params[:receiver_ids].uniq.each do |receiver_id|
+      message_params[:receiver_ids].uniq.each do |receiver_id|
         return unless User.exists?(id: receiver_id)
 
-        @message = Message.new(body: @message_params[:body], sender_id: @message_params[:sender_id],
+        @message = Message.new(body: message_params[:body], sender_id: message_params[:sender_id],
                                receiver_id: receiver_id)
-        unless @message.save
-          is_successful = false
-          raise ActiveRecord::Rollback
-        end
+        raise ActiveRecord::Rollback unless @message.save
       end
     end
-    is_successful
+    true
   end
 end
